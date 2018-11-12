dcff845ee088fdb4373a413c02ff38cb902825c6
Nicholas Hernandez <nicholas.john.hernandez@gmail.com>
2018-11-12 14:52:31 -0800

    added hotel manager tables

---
+++ SQL/hotel_manager_tables.sql
@@ -0,0 +1,55 @@
+DROP TABLE IF EXISTS `room_availability`;
+DROP TABLE IF EXISTS `reservation`;
+DROP TABLE IF EXISTS `guest`;
+CREATE TABLE guest (
+    guest_id         INTEGER           not null,
+    first_name        VARCHAR(50)       not null,
+    last_name         VARCHAR(50)       not null,
+    email             VARCHAR(50)       not null,
+   PRIMARY KEY (guest_id)
+);
+
+DROP TABLE IF EXISTS `room`;
+CREATE TABLE room (
+    room_id        INTEGER          not null,
+    room_type      VARCHAR(50)      not null,
+    tv             BOOLEAN          not null,
+    hot_tub        BOOLEAN          not null,
+    PRIMARY KEY ( room_id )
+);
+
+
+CREATE TABLE reservation (
+    reservation_id     INTEGER          not null,
+    room_id            INTEGER          not null,
+    guest_id          INTEGER          not null, 
+    phone              VARCHAR(20)      not null,
+    arrive             DATE             not null,
+    depart             DATE             not null,
+    cost               DOUBLE           not null,
+    reservation_status VARCHAR(50)      not null,
+    payment_due        DATE             not null,
+    payment_id         INTEGER          not null,
+    FOREIGN KEY ( guest_id ) REFERENCES guest (guest_id),
+    FOREIGN KEY ( room_id ) REFERENCES room(room_id),
+    PRIMARY KEY ( reservation_id )
+);
+
+DROP TABLE IF EXISTS `payment`;
+CREATE TABLE payment(
+   payment_id         INTEGER         not null,
+   credit_card        VARCHAR(16)     not null,
+   experation_date    DATE            not null,
+   amount_due         DOUBLE          not null,
+   PRIMARY KEY ( payment_id )
+);
+
+
+
+DROP TABLE IF EXISTS `room_availability`;
+CREATE TABLE room_availability (
+    room_id             INTEGER            not null,
+    start_date          DATE               not null,
+    end_date            DATE               not null,
+    FOREIGN KEY ( room_id ) REFERENCES room (room_id)
+);
