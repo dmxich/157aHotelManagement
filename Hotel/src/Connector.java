e1b4e0f75db7626f698987fe4eda4ad37b6ebbf2
Yunjun Ma <yunjunma@gmail.com>
2018-11-16 13:06:11 -0800

    Adding a Connector

---
+++ Hotel/src/Connector.java
@@ -0,0 +1,22 @@
+import java.sql.*;
+
+
+public class Connector {
+
+	// JDBC driver name and database URL
+	   static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+	//   static final String DB_URL = "jdbc:mysql://localhost/library";
+	   static final String DB_URL = ;
+
+
+	   //  Database credentials
+	   static final String USER = ;
+	   static final String PASS = ;
+	   
+	   public static void main(String[] args) {
+	   Connection conn = null;
+	   Statement stmt = null;
+	   ResultSet rs = null;
+	   
+	   }
+}
