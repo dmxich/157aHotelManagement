7c5f9ccee03b410db279da398671fe92c0ab5291
Yunjun Ma <yunjunma@gmail.com>
2018-11-16 13:25:35 -0800

    Added a database source Connector is done

--- Hotel/src/Connector.java
+++ Hotel/src/Connector.java
@@ -1,22 +1,58 @@
-import java.sql.*;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+import javax.sql.DataSource;
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 
 
-public class Connector {
+class DataSourceFactory {
+    public static DataSource getMySQLDataSource() {
+        
+    	Properties props = new Properties();
+        FileInputStream fis = null;
+        MysqlDataSource mysqlDS = null;
+        try {
+            fis = new FileInputStream("db.properties");
+            props.load(fis);
+            mysqlDS = new MysqlDataSource();
+            mysqlDS.setURL(props.getProperty("MYSQL_DB_URL"));
+            mysqlDS.setUser(props.getProperty("MYSQL_DB_USERNAME"));
+            mysqlDS.setPassword(props.getProperty("MYSQL_DB_PASSWORD"));
+        } catch (IOException e) {
+        	System.out.println("db.properties is not found");
+            e.printStackTrace();
+        }
+        return mysqlDS;
+      
+    }
+}
 
-	// JDBC driver name and database URL
-	   static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
-	//   static final String DB_URL = "jdbc:mysql://localhost/library";
-	   static final String DB_URL = ;
 
-
-	   //  Database credentials
-	   static final String USER = ;
-	   static final String PASS = ;
-	   
-	   public static void main(String[] args) {
-	   Connection conn = null;
-	   Statement stmt = null;
-	   ResultSet rs = null;
-	   
-	   }
+public class Connector 
+{
+ public static void main (String [] args)
+ {
+        DataSource ds = DataSourceFactory.getMySQLDataSource();     
+       
+        Connection connection =  null; 
+        try {
+    		connection = ds.getConnection(); 
+    	} catch (SQLException e) {
+    		System.out.println("Connection Failed! Check output console");
+    		e.printStackTrace();
+    		return;
+    	}
+     
+    	if (connection != null) {
+    		System.out.println("You made it, take control your database now!");
+    	} else {
+    		System.out.println("Failed to make connection!");
+    	}
+ }        
 }
