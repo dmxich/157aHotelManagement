39aff44e63ff2c21d6695e179cd6a5667191ed80
yunjunma <yunjunma@gmail.com>
2018-11-26 12:42:22 -0800

    Added Jun's stored procedures. Tested, whole sql work

--- SQL/hotel_manager_tables.sql
+++ SQL/hotel_manager_tables.sql
@@ -12,7 +12,6 @@ CREATE TABLE user (
 );
 ALTER TABLE user MODIFY COLUMN user_id INT NOT NULL AUTO_INCREMENT;
 
-
 DROP TABLE IF EXISTS `room`;
 CREATE TABLE room (
     room_id        INTEGER          not null,
@@ -59,14 +58,94 @@ CREATE TABLE room_reserved (
     start_date	    	DATE		   	not null,
     end_date	    	DATE		   	not null,
     PRIMARY KEY ( room_id, start_date ),
-    FOREIGN KEY ( room_id ) REFERENCES room (room_id),
+    FOREIGN KEY ( room_id ) REFERENCES room (room_id)
 );
+
+USE `hotel`;
+DROP procedure IF EXISTS `spMakeReservation`;
+
+DELIMITER $$
+USE `hotel`$$
+CREATE DEFINER=`hotel`@`%` PROCEDURE `spMakeReservation`(
+IN rm_id INTEGER, 
+IN u_id INTEGER, 
+IN phone VARCHAR(20), 
+IN arrive DATE, 
+IN depart DATE, 
+IN cost DOUBLE, 
+IN r_status VARCHAR(50), 
+IN payment_due DATE, 
+IN card_number VARCHAR(16),
+IN expiration DATE,
+IN amount DOUBLE,
+IN p_id INTEGER)
+BEGIN
+	INSERT INTO payment 
+	(credit_card, expiration_date, amount_due)
+	VALUES
+	( card_number, expiration, amount);
+
+    INSERT INTO reservation 
+    ( room_id, user_id, phone, arrive, depart, cost, r_status, payment_due, payment_id) 
+	VALUES
+	(rm_id, u_id, phone, arrive, depart, cost, r_status, payment_due, p_id);
+    
+    INSERT INTO room_reserved
+    ( room_id, start_date, end_date)
+    VALUES 
+    ( rm_id, arrive, depart);
+END$$
+
+DELIMITER ;
+
+USE `hotel`;
+DROP procedure IF EXISTS `spMakePayment`;
+
+DELIMITER $$
+USE `hotel`$$
+CREATE DEFINER=`hotel`@`%` PROCEDURE `spMakePayment`(IN reservationID INTEGER)
+BEGIN
+UPDATE payment
+SET paid = true
+WHERE payment.payment_id IN
+		    (SELECT payment_id
+		    FROM reservation
+		    WHERE reservation_id = reservationID);
+UPDATE reservation 
+SET 
+    r_status = 'reserved'
+WHERE
+    reservation_id = reservationID;
+END$$
+
+DELIMITER ;
+
+USE `hotel`;
+DROP procedure IF EXISTS `spUpdatePayment`;
+
+DELIMITER $$
+USE `hotel`$$
+CREATE DEFINER=`hotel`@`%` PROCEDURE `spUpdatePayment`(IN reservationID INTEGER,
+ IN card_number VARCHAR(18),
+IN date DATE
+) 
+BEGIN
+UPDATE payment
+SET credit_card = card_number, expiration_date = date
+WHERE payment.payment_id IN
+		    (SELECT P.payment_id
+		    FROM reservation P
+		    WHERE P.reservation_id = reservationID);
+END$$
+
+DELIMITER ;
+
 
 
 INSERT INTO user (user_id, first_name, last_name, email, isAdmin, password) 
 VALUES (10001, 'Jun', 'Ma', 'jun@gmail.com', false, 123456);
 INSERT INTO user (user_id, first_name, last_name, email, isAdmin, password) 
-VALUES (10001, 'A', 'Admin', 'admin@gmail.com', true, 123456);
+VALUES (10002, 'A', 'Admin', 'admin@gmail.com', true, 123456);
 
 INSERT INTO room ( room_id, room_type, tv, hot_tub, rate) 
 VALUES 
@@ -94,5 +173,3 @@ VALUES (20001, 101, 10001, '4151111134', '2018-06-10', '2018-06-12', 460.2, 'pen
 
 INSERT INTO room_reserved (room_id, start_date, end_date) 
 VALUES (101, '2018-06-10', '2018-06-12');
-
-
